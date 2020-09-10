111a216f68f846dae6fe35cc10300bae77084eea
sean <seanzq0331@163.com>
2020-09-11 00:08:43 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+/*
+ * electron 模块所提供的功能都是通过命名空间暴露出来的。 比如说： electron.app负责管理Electron 应用程序的生命周期，
+ * electron.BrowserWindow类负责创建窗口。 下面是一个简单的main.js文件，它将在应用程序准备就绪后打开一个窗口
+ */
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // 创建浏览器窗口
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // 并且为你的应用加载index.html
+  win.loadFile('index.html')
+
+  // 打开开发者工具
+win.webContents.openDevTools()
+
+}
+
+// Electron会在初始化完成并且准备好创建浏览器窗口时调用这个方法
+// 部分 API 在 ready 事件触发后才能使用。
+app.whenReady().then(createWindow)
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
+// 您可以把应用程序其他的流程写在在此文件中
+// 代码 也可以拆分成几个文件，然后用 require 导入。
---
+++ records.md
@@ -0,0 +1,22 @@
+# nvm nodejs 升级
+#### - 查看nvm,node,npm 版本  
+  mvm --version  
+  node -v   
+  npm -v   
+
+#### - 查看node npm 对应版本 ![]('https://nodejs.org/zh-cn/download/releases/')
+
+#### - 安装nrm(npm registry manager 也就是npm的镜像源管理工具)  
+  npm install -g nrm
+  1. 命令：nrm ls 用于展示所有可切换的镜像地址
+  2. 命令：nrm use cnpm 我们这样就可以直接切换到cnpm上了。当然也可以按照上面罗列的其他内容进行切换
+  3. 命令：nrm test npm 测试当前镜像速度
+
+
+项目下新建 .npmrc 文件，加入如下配置：
+
+electron_mirror=https://npm.taobao.org/mirrors/electron/
+即使用淘宝的源，重新 npm install
+
+npm install --save-dev electron
+如果速度还是很慢就把  package-lock.json 和 node-modules 删掉，重新 install
